851e8a2995a8737516965363f260695172a37019
Raghavendra S I <raghavendras715@gmail.com>
2025-01-29 22:34:02 +0530

    updated code on 29/01

--- internal/config/http/handlers/student.go
+++ internal/config/http/handlers/student.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/Raghavendra/students-api/internal/types"
 	"github.com/Raghavendra/students-api/internal/utils/response"
+	"github.com/go-playground/validator/v10"
 )
 
 func New() http.HandlerFunc {
@@ -23,6 +24,20 @@ func New() http.HandlerFunc {
 			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
 			return
 		}
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		//request validation
+
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			return
+		}
+
 		slog.Info("creating a student")
 
 		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "ok"})
